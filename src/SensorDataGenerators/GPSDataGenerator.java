d80947f79d4925cd912e4c824fc59a9c985df691
Placu <andrea.placuzzi@studio.unibo.it>
2019-10-17 15:58:01 +0200

    add GPS sensor

---
+++ src/SensorDataGenerators/GPSDataGenerator.java
@@ -0,0 +1,17 @@
+package SensorDataGenerators;
+
+import util.MapHelper;
+
+import java.nio.ByteBuffer;
+import java.time.LocalTime;
+
+public class GPSDataGenerator implements SensorDataGenerator {
+
+    @Override
+    public byte[] generateData(Integer x, Integer y, LocalTime time) {
+        byte[] data= new byte[16];
+        ByteBuffer.wrap(data, 0, 8).putDouble(MapHelper.getInstance().toLatitude(y));
+        ByteBuffer.wrap(data, 8, 8).putDouble(MapHelper.getInstance().toLongitude(x));
+        return data;
+    }
+}
