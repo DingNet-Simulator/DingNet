fc48d6327871fd876a89d5bd9df39769d3c9c978
Placu <andrea.placuzzi@studio.unibo.it>
2019-10-18 14:59:41 +0200

    change from double to float

--- src/SensorDataGenerators/GPSDataGenerator.java
+++ src/SensorDataGenerators/GPSDataGenerator.java
@@ -10,9 +10,9 @@ public class GPSDataGenerator implements SensorDataGenerator {
 
     @Override
     public byte[] generateData(Integer x, Integer y, LocalTime time) {
-        byte[] data= new byte[16];
-        ByteBuffer.wrap(data, 0, 8).putDouble(MapHelper.getInstance().toLatitude(y));
-        ByteBuffer.wrap(data, 8, 8).putDouble(MapHelper.getInstance().toLongitude(x));
+        byte[] data= new byte[8];
+        ByteBuffer.wrap(data, 0, 4).putFloat((float)MapHelper.getInstance().toLatitude(y));
+        ByteBuffer.wrap(data, 4, 4).putFloat((float)MapHelper.getInstance().toLongitude(x));
         return data;
     }
     public byte[] generateData(Pair<Integer, Integer> pos, LocalTime time){
