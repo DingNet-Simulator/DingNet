286bf2408119db5c3789e732aeca7648b34ecc48
Placu <andrea.placuzzi@studio.unibo.it>
2019-10-18 09:53:38 +0200

    modify payload

--- src/IotDomain/mqtt/MqttMessage.java
+++ src/IotDomain/mqtt/MqttMessage.java
@@ -1,6 +1,5 @@
 package IotDomain.mqtt;
 
-import java.util.LinkedList;
 import java.util.List;
 
 public class MqttMessage {
@@ -8,18 +7,13 @@ public class MqttMessage {
     private final long deviceEUI;
     private final long gatewayEUI;
     private final long applicationEUI;
-    private final double latitude;
-    private final double longitude;
-    private final List<Integer> sensedData;
+    private final List<Byte> data;
 
     public MqttMessage(List<Byte> packet, long deviceEUI, long gatewayEUI, long applicationEUI) {
         this.deviceEUI = deviceEUI;
         this.gatewayEUI = gatewayEUI;
         this.applicationEUI = applicationEUI;
-        //TODO decode packet
-        latitude = 0;
-        longitude = 0;
-        sensedData = new LinkedList<>();
+        data = packet;
     }
 
     public long getDeviceEUI() {
@@ -34,15 +28,7 @@ public class MqttMessage {
         return applicationEUI;
     }
 
-    public double getLatitude() {
-        return latitude;
-    }
-
-    public double getLongitude() {
-        return longitude;
-    }
-
-    public List<Integer> getSensedData() {
-        return sensedData;
+    public List<Byte> getData() {
+        return data;
     }
 }
