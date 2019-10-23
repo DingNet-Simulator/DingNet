3a2b4931f5ab02fa9cb7e94e5fc0ef8114c3ee78
Placu <andrea.placuzzi@studio.unibo.it>
2019-10-23 14:25:00 +0200

    add response to mote

--- src/IotDomain/Gateway.java
+++ src/IotDomain/Gateway.java
@@ -1,6 +1,8 @@
 package IotDomain;
 
 
+import IotDomain.gatewayresponsestrategy.NoResponse;
+import IotDomain.gatewayresponsestrategy.ResponseStrategy;
 import IotDomain.mqtt.MqttClientBasicApi;
 import IotDomain.mqtt.MqttMessage;
 import IotDomain.mqtt.MqttMock;
@@ -16,6 +18,7 @@ public class Gateway extends NetworkEntity {
 
     private LinkedList<MoteProbe> subscribedMoteProbes;
     private final MqttClientBasicApi mqttClient;
+    private final ResponseStrategy responseStrategy;
 
     /**
      * A construtor creating a gateway with a given xPos, yPos, environment and transmission power.
@@ -27,10 +30,24 @@ public class Gateway extends NetworkEntity {
      * @Effect creates a gateway with a given name, xPos, yPos, environment and transmission power.
      */
     public Gateway(Long gatewayEUI, Integer xPos, Integer yPos, Environment environment, Integer transmissionPower, Integer SF) {
+        this(new NoResponse(), gatewayEUI, xPos, yPos, environment, transmissionPower, SF);
+    }
+    /**
+     * A construtor creating a gateway with a given xPos, yPos, environment and transmission power.
+     * @param responseStrategy strategy to enable response to mote
+     * @param gatewayEUI gateway identifier.
+     * @param xPos  The x-coordinate of the gateway on the map.
+     * @param yPos  The y-coordinate of the gateway on the map.
+     * @param environment   The map of the environment.
+     * @param transmissionPower   The transmission power of the gateway.
+     * @Effect creates a gateway with a given name, xPos, yPos, environment and transmission power.
+     */
+    public Gateway(ResponseStrategy responseStrategy, Long gatewayEUI, Integer xPos, Integer yPos, Environment environment, Integer transmissionPower, Integer SF) {
         super(gatewayEUI , xPos, yPos, environment, transmissionPower, SF, 1.0);
         environment.addGateway(this);
         subscribedMoteProbes = new LinkedList<>();
         mqttClient = new MqttMock();
+        this.responseStrategy = responseStrategy.init(this);
     }
 
     /**
@@ -55,12 +72,15 @@ public class Gateway extends NetworkEntity {
      */
     @Override
     protected void OnReceive(Byte[] packet, Long senderEUI, Long designatedReceiver) {
-        var message = new MqttMessage(new LinkedList<>(Arrays.asList(packet)), designatedReceiver, senderEUI,getEUI());
-        mqttClient.publish(getTopic(designatedReceiver, senderEUI), message);
-        for (MoteProbe moteProbe : getSubscribedMoteProbes()){
-            moteProbe.trigger(this,senderEUI);
+        //manage the message only if it is of a mote
+        if (getEnvironment().getMotes().stream().anyMatch(m -> m.getEUI().equals(senderEUI))) {
+            var message = new MqttMessage(new LinkedList<>(Arrays.asList(packet)), designatedReceiver, senderEUI, getEUI());
+            mqttClient.publish(getTopic(designatedReceiver, senderEUI), message);
+            for (MoteProbe moteProbe : getSubscribedMoteProbes()) {
+                moteProbe.trigger(this, senderEUI);
+            }
+            responseStrategy.retrieveResponse(designatedReceiver, senderEUI).ifPresent(this::loraSend);
         }
-
     }
 
     private String getTopic(Long applicationEUI, Long deviceEUI) {
