95a1321bab98967f0a3ef82b7b1087cf67ec6ecc
Placu <andrea.placuzzi@studio.unibo.it>
2019-10-30 17:09:45 +0100

    add the new path to the previous one

--- src/IotDomain/motepacketstrategy/consumeStrategy/ReplacePathWithMiddlePoints.java
+++ src/IotDomain/motepacketstrategy/consumeStrategy/ReplacePathWithMiddlePoints.java
@@ -2,11 +2,10 @@ package IotDomain.motepacketstrategy.consumeStrategy;
 
 import IotDomain.lora.LoraWanPacket;
 import IotDomain.networkentity.Mote;
-import util.PathWithMiddlePoints;
 
 public class ReplacePathWithMiddlePoints extends ReplacePath {
     @Override
     public void consume(Mote mote, LoraWanPacket packet) {
-        mote.setPath(new PathWithMiddlePoints(extractPath(packet)));
+        mote.getPath().addPositions(extractPath(packet));
     }
 }
