247af34d927271211c492fd064eb89fb63366431
Placu <andrea.placuzzi@studio.unibo.it>
2019-11-13 15:45:14 +0100

    remove unused file

--- src/main/java/iot/strategy/collision/CollisionStrategy.java
+++
@@ -1,8 +0,0 @@
-package iot.strategy.collision;
-
-import iot.networkcommunication.LoraTransmission;
-
-public interface CollisionStrategy {
-
-    void manageCollision(LoraTransmission loraTransmission);
-}
