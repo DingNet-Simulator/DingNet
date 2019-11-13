6b1b5ddd9d8dd05c745d72a15d2f314dadb831d3
Placu <andrea.placuzzi@studio.unibo.it>
2019-11-13 10:05:41 +0100

    transform in beans

---
+++ src/main/java/IotDomain/networkcommunication/LoraWanPacket.java
@@ -0,0 +1,183 @@
+package IotDomain.networkcommunication;
+
+import IotDomain.lora.BasicFrameHeader;
+import IotDomain.lora.FrameHeader;
+import IotDomain.lora.MacCommand;
+import util.Converter;
+
+import java.io.Serializable;
+import java.util.LinkedList;
+import java.util.List;
+
+public class LoraWanPacket implements Serializable, Packet{
+    private static final long serialVersionUID = 1L;
+    /**
+     * The payload of a packet.
+     */
+    private final Byte[] payload;//TODO change to row type
+    /**
+     * if the packet has low data rate optimization.
+     */
+    private final Boolean lowDataRateOptimization;
+    /**
+     * The coding rate of the packet
+     */
+    private final double codingRate;
+    /**
+     * The amount of preamble symbols of the packet
+     */
+    private final Integer amountOfPreambleSymbols;
+    /**
+     * The length in symbols of the packet.
+     */
+    private final Integer length;
+    /**
+     * The MAC commands included in the packet.
+     */
+    private final LinkedList<MacCommand> macCommands;
+    /**
+     * The EUI of designated receiver.
+     */
+    private final Long designatedReceiverEUI;
+    /**
+     * The EUI of the sender.
+     */
+    private final Long senderEUI;
+
+    private final FrameHeader header;
+
+    //region constructor
+    /**
+     * A constructor generating a packet with a given payload, header, lowDataRateOptimization, amountOfPreambleSymbols,
+     * codingRate and macCommands.
+     * @param senderEUI
+     * @param designatedReceiverEUI
+     * @param payload
+     * @param header
+     * @param lowDataRateOptimization
+     * @param amountOfPreambleSymbols
+     * @param codingRate
+     * @param macCommands
+     */
+    public LoraWanPacket(Long senderEUI, Long designatedReceiverEUI, Byte[] payload, FrameHeader header, Boolean lowDataRateOptimization,
+                         Integer amountOfPreambleSymbols, double codingRate, LinkedList<MacCommand> macCommands){
+        this.senderEUI = senderEUI;
+        this.designatedReceiverEUI = designatedReceiverEUI;
+        this.amountOfPreambleSymbols = amountOfPreambleSymbols;
+        this.codingRate = codingRate;
+        this.header = header;
+        this.lowDataRateOptimization = lowDataRateOptimization;
+        this.payload = payload;
+        this.macCommands = macCommands;
+        this.length = payload.length + 13;
+    }
+
+    /**
+     * A constructor generating a packet with a given payload and macCommands;
+     * @param senderEUI
+     * @param designatedReceiverEUI
+     * @param payload
+     * @param header
+     * @param macCommands
+     */
+    public LoraWanPacket(Long senderEUI, Long designatedReceiverEUI, Byte[] payload, FrameHeader header, LinkedList<MacCommand> macCommands){
+        this(senderEUI, designatedReceiverEUI, payload, header,false,8,0.8,macCommands);
+    }
+
+    /**
+     * A constructor generating a packet with a given payload and macCommands;
+     * @param senderEUI
+     * @param designatedReceiverEUI
+     * @param payload
+     * @param macCommands
+     */
+    public LoraWanPacket(Long senderEUI, Long designatedReceiverEUI, Byte[] payload, LinkedList<MacCommand> macCommands) {
+        this(senderEUI, designatedReceiverEUI, payload, new BasicFrameHeader(), false, 8, 0.8, macCommands);
+    }
+
+    public static LoraWanPacket createEmptyPacket(Long senderEUI, Long designatedReceiverEUI) {
+        return new LoraWanPacket(senderEUI, designatedReceiverEUI, new Byte[0], new LinkedList<>());
+    }
+
+    //endregion
+
+    //TODO
+    public byte[] getHeader() {
+        return new byte[0];
+    }
+
+    public FrameHeader getFrameHeader() {return header;}
+
+    /**
+     * Returns the MAC commands
+     * @return the MAC commands.
+     */
+    public List<MacCommand> getMacCommands(){
+        return macCommands;
+    }
+
+    /**
+     * Returns the payload
+     * @return the payload
+     */
+    public byte[] getPayload() {
+        return Converter.toRowType(payload);
+    }
+
+    /**
+     * Returns the EUI of designated receiver.
+     * @return The EUI of designated receiver.
+     */
+    @Override
+    public long getReceiverEUI() {
+        return designatedReceiverEUI;
+    }
+
+    /**
+     * Returns the EUI of sender.
+     * @return The EUI of sender.
+     */
+    public long getSenderEUI() {
+        return senderEUI;
+    }
+
+    /**
+     * Returns if the packet has an explicit header.
+     * @return if the packet has an explicit header.
+     */
+    public boolean hasHeader() {
+        return true;//TODO
+    }
+
+    /**
+     * Returns if the packet has low data rate optimization.
+     * @return if the packet has low data rate optimization.
+     */
+    public boolean hasLowDataRateOptimization() {
+        return lowDataRateOptimization;
+    }
+
+    /**
+     * Returns the coding rate.
+     * @return the coding rate.
+     */
+    public double getCodingRate() {
+        return codingRate;
+    }
+
+    /**
+     * Returns the amount of preamble symbols.
+     * @return the amount of preamble symbols.
+     */
+    public int getAmountOfPreambleSymbols() {
+        return amountOfPreambleSymbols;
+    }
+
+    /**
+     * Returns the length in symbols.
+     * @return the length in symbols.
+     */
+    public int getLength(){
+        return length;
+    }
+}
