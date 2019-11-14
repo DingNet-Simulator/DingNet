a1c0dc0c26931e86bb5ac72d63bbfc01009bda2c
Placu <andrea.placuzzi@studio.unibo.it>
2019-11-14 15:50:00 +0100

    add interface for mqtt Message

---
+++ src/main/java/iot/mqtt/BasicMqttMessage.java
@@ -0,0 +1,47 @@
+package iot.mqtt;
+
+import iot.lora.FrameHeader;
+import util.Converter;
+
+import java.util.List;
+
+public class BasicMqttMessage implements MqttMessage {
+
+    private final long deviceEUI;
+    private final long gatewayEUI;
+    private final long applicationEUI;
+    private final List<Byte> data;
+    private final FrameHeader header;
+
+    public BasicMqttMessage(FrameHeader header, List<Byte> packet, long deviceEUI, long gatewayEUI, long applicationEUI) {
+        this.deviceEUI = deviceEUI;
+        this.gatewayEUI = gatewayEUI;
+        this.applicationEUI = applicationEUI;
+        data = packet;
+        this.header = header;
+    }
+
+    public long getDeviceEUI() {
+        return deviceEUI;
+    }
+
+    public long getGatewayEUI() {
+        return gatewayEUI;
+    }
+
+    public long getApplicationEUI() {
+        return applicationEUI;
+    }
+
+    public List<Byte> getData() {
+        return data;
+    }
+
+    public byte[] getDataAsArray() {
+        return Converter.toRowType(data.toArray(new Byte[0]));
+    }
+
+    public FrameHeader getHeader() {
+        return header;
+    }
+}
