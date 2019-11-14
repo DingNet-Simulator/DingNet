62d06de8303233b5e93e9e92632d35f889f36c03
Placu <andrea.placuzzi@studio.unibo.it>
2019-11-14 17:42:48 +0100

    add app to gateway behaviour

--- src/main/java/iot/networkentity/NetworkServer.java
+++ src/main/java/iot/networkentity/NetworkServer.java
@@ -1,6 +1,5 @@
 package iot.networkentity;
 
-
 import iot.lora.LoraTransmission;
 import iot.lora.LoraWanPacket;
 import iot.mqtt.MqttClientBasicApi;
@@ -8,6 +7,7 @@ import iot.mqtt.Topics;
 import iot.mqtt.TransmissionWrapper;
 
 import java.util.*;
+import java.util.function.BinaryOperator;
 
 public class NetworkServer {
 
@@ -15,11 +15,24 @@ public class NetworkServer {
     private final Map<Long, Map<Long, LoraTransmission<LoraWanPacket>>> transmissionReceived;
     private final Map<Long, List<LoraTransmission<LoraWanPacket>>> historyMote;
     private final MqttClientBasicApi mqttClient;
+    private BinaryOperator<Map.Entry<Long, LoraTransmission<LoraWanPacket>>> chooseGatewayStrategy = this::chooseByTransmissionPower;
 
     public NetworkServer(MqttClientBasicApi mqttClient) {
         this.mqttClient = mqttClient;
         transmissionReceived = new HashMap<>();
         historyMote = new HashMap<>();
+        subscribeToGateways();
+        subscribeToApps();
+    }
+
+    public void reset() {
+        transmissionReceived.clear();
+        historyMote.clear();
+    }
+
+    public NetworkServer setChooseGatewayStrategy(BinaryOperator<Map.Entry<Long, LoraTransmission<LoraWanPacket>>> strategy) {
+        chooseGatewayStrategy = strategy;
+        return this;
     }
 
     private void subscribeToGateways() {
@@ -56,13 +69,35 @@ public class NetworkServer {
         return Long.valueOf(list.get(index + 1));
     }
 
+    //TODO add check
+    private long getApp(String topic) {
+        var list = Arrays.asList(topic.split("/"));
+        var index = list.indexOf("application");
+        return Long.valueOf(list.get(index + 1));
+    }
+
     private void subscribeToApps() {
         mqttClient.subscribe(Topics.getAppToNetServer("+", "+"),
             (topic, msg) -> {
                 //get mote id
                 var moteId = getMote(topic);
                 //find best gateway
-                //send to best gateway
+                if (transmissionReceived.containsKey(moteId)) {
+                    var gatewayId = transmissionReceived.get(moteId)
+                        .entrySet()
+                        .stream()
+                        .reduce((e1, e2) -> chooseGatewayStrategy.apply(e1,e2))
+                        .map(Map.Entry::getKey)
+                        .orElseThrow(() -> new IllegalStateException("no gateway available for the mote: " + moteId));
+                    //send to best gateway
+                    mqttClient.publish(Topics.getNetServerToGateway(getApp(topic), gatewayId, moteId), msg);
+                }
             });
+    }
+
+    private Map.Entry<Long, LoraTransmission<LoraWanPacket>> chooseByTransmissionPower(
+            Map.Entry<Long, LoraTransmission<LoraWanPacket>> e1,
+            Map.Entry<Long, LoraTransmission<LoraWanPacket>> e2) {
+        return e1.getValue().getTransmissionPower() >= e2.getValue().getTransmissionPower() ? e1 : e2;
     }
 }
