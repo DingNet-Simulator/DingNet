9566f8e36bcb34ad56b2fff4aa4312932d7aae8a
Placu <andrea.placuzzi@studio.unibo.it>
2019-11-18 13:42:26 +0100

    add adapters to serialize/deserialize interface

--- src/main/java/iot/mqtt/PahoMqttClient.java
+++ src/main/java/iot/mqtt/PahoMqttClient.java
@@ -1,13 +1,17 @@
 package iot.mqtt;
 
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
+import com.google.gson.*;
+import iot.lora.BasicFrameHeader;
+import iot.lora.EU868ParameterByDataRate;
+import iot.lora.FrameHeader;
+import iot.lora.RegionalParameter;
 import org.eclipse.paho.client.mqttv3.MqttClient;
 import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
 import org.eclipse.paho.client.mqttv3.MqttException;
 import org.eclipse.paho.client.mqttv3.persist.MemoryPersistence;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.Base64;
 import java.util.function.BiConsumer;
 
 import static java.nio.charset.StandardCharsets.US_ASCII;
@@ -15,20 +19,47 @@ import static java.nio.charset.StandardCharsets.US_ASCII;
 public class PahoMqttClient implements MqttClientBasicApi{
 
     private MqttClient mqttClient;
-    private final Gson gson;
+    private Gson gson;
 
     public PahoMqttClient() {
-        this("tcp://test.mosquitto.org:1883", "testFenomeno");
+        this("tcp://mqtt.eclipse.org:1883", "testFenomeno1995");
     }
 
     public PahoMqttClient(@NotNull String address, @NotNull String clientId) {
-        gson = new GsonBuilder().create();
+        gson = addAdapters(new GsonBuilder()).create();
         try {
-            this.mqttClient = new MqttClient(address, clientId, new MemoryPersistence());
+            mqttClient = new MqttClient(address, clientId, new MemoryPersistence());
             connect();
         } catch (MqttException e) {
             e.printStackTrace();
         }
+    }
+
+    private GsonBuilder addAdapters(GsonBuilder builder) {
+
+        builder.registerTypeAdapter(FrameHeader.class, (JsonSerializer<FrameHeader>) (header, type, context) -> {
+            var obj = new JsonObject();
+            obj.addProperty("sourceAddress", Base64.getEncoder().encodeToString(header.getSourceAddress()));
+            obj.addProperty("fCtrl", header.getFCtrl());
+            obj.addProperty("fCnt", header.getFCntAsShort());
+            obj.addProperty("fOpts", Base64.getEncoder().encodeToString(header.getFOpts()));
+            return obj;
+        });
+
+        builder.registerTypeAdapter(FrameHeader.class, (JsonDeserializer<FrameHeader>) (jsonElement, type, jsonDeserializationContext) -> {
+            var header = new BasicFrameHeader();
+            header
+                .setSourceAddress(Base64.getDecoder().decode(((JsonObject) jsonElement).get("sourceAddress").getAsString()))
+                .setFCnt(((JsonObject) jsonElement).get("fCnt").getAsShort())
+                .setfCtrl(((JsonObject) jsonElement).get("fCtrl").getAsByte())
+                .setFOpts(Base64.getDecoder().decode(((JsonObject) jsonElement).get("fOpts").getAsString()));
+            return header;
+        });
+
+        builder.registerTypeAdapter(RegionalParameter.class,
+            (JsonDeserializer<RegionalParameter>) (element, type, context) -> EU868ParameterByDataRate.valueOf(element.getAsString()));
+
+        return builder;
     }
 
     @Override
@@ -55,6 +86,9 @@ public class PahoMqttClient implements MqttClientBasicApi{
     public void publish(String topic, MqttMessage message) {
         var msg = new org.eclipse.paho.client.mqttv3.MqttMessage(gson.toJson(message).getBytes(US_ASCII));
         try {
+            if(!mqttClient.isConnected()) {
+                connect();
+            }
             mqttClient.publish(topic, msg);
         } catch (MqttException e) {
             e.printStackTrace();
