7752f36b7f1a7425746a73327280e14fc2c1fa0b
Placu <andrea.placuzzi@studio.unibo.it>
2019-11-18 13:51:37 +0100

    add strategy to forward packet to device when it arrived

---
+++ src/main/java/iot/strategy/response/gateway/SendPacketImmediately.java
@@ -0,0 +1,38 @@
+package iot.strategy.response.gateway;
+
+
+import iot.lora.LoraWanPacket;
+import iot.mqtt.LoraWanPacketWrapper;
+import iot.mqtt.Topics;
+import iot.networkentity.Gateway;
+import util.Pair;
+
+import java.util.Optional;
+
+public class SendPacketImmediately implements ResponseStrategy {
+
+    private Gateway gateway;
+
+    @Override
+    public ResponseStrategy init(Gateway gateway) {
+        this.gateway = gateway;
+        //subscribe to all mote topic
+        subscribeToMotesTopic();
+        return this;
+    }
+
+    private void subscribeToMotesTopic() {
+        gateway.getEnvironment().getMotes().stream()
+            .map(m -> new Pair<>(m.getApplicationEUI(), m.getEUI()))
+            .forEach(m -> gateway.getMqttClient().subscribe(
+                Topics.getNetServerToGateway(m.getLeft(), gateway.getEUI(), m.getRight()),
+                LoraWanPacketWrapper.class,
+                (t, msg) -> gateway.sendToDevice(msg.getPacket())
+            ));
+    }
+
+    @Override
+    public Optional<LoraWanPacket> retrieveResponse(Long applicationEUI, Long deviceEUI) {
+        return Optional.empty();
+    }
+}
