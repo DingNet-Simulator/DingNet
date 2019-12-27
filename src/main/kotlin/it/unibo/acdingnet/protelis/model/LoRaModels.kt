3483019d816f9e252fef0d228b05187c624d91bd
Placu <andrea.placuzzi@studio.unibo.it>
2019-12-27 14:48:05 +0100

    add lora transmission model

---
+++ src/main/kotlin/it/unibo/acdingnet/protelis/model/LoRaModels.kt
@@ -0,0 +1,35 @@
+package it.unibo.acdingnet.protelis.model
+
+import java.time.LocalTime
+
+data class FrameHeaderApp (
+    val sourceAddress: List<Byte>,
+    val fCtrl: Int,
+    val fCnt: Int,
+    val fOpts: List<Byte>
+)
+
+data class LoRaWanPacket (
+    val senderEUI: Long,
+    val designatedReceiverEUI: Long,
+    val lowDataRateOptimization: Boolean,
+    val codingRate: Double,
+    val length: Int,
+    val payload: List<Byte>,
+    val macCommands: List<String>,
+    val header: FrameHeaderApp
+)
+
+data class LoRaTransmission (
+    val sender: Long,
+    val receiver: Long,
+    val transmissionPower: Double,
+    val xPos: Int,
+    val yPos: Int,
+    val content: LoRaWanPacket,
+    val regionalParameter: String,
+    val departureTime: LocalTime,
+    val timeOnAir: Double,
+    val arrived: Boolean,
+    val collided: Boolean
+)
