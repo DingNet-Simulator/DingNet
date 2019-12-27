ca04a008a0c4d5e8ea694bd79a81c99f8f1d98b5
Placu <andrea.placuzzi@studio.unibo.it>
2019-12-27 14:51:12 +0100

    add util classes

---
+++ src/main/kotlin/it/unibo/acdingnet/protelis/util/Const.kt
@@ -0,0 +1,15 @@
+package it.unibo.acdingnet.protelis.util
+
+object Const {
+
+    const val APPLICATION_ID = "1"
+    const val NEIGHBORHOOD_RANGE = 1000.0
+    const val MOTT_ADDRESS = "tcp://test.mosquitto.org:1883"
+    const val MQTT_CLIENT_ID = "testFenomeno1995-app"
+
+    object ProtelisEnv {
+        const val IAQLEVEL_KEY = "iaqLevel"
+        const val SOURCE_KEY = ""
+        const val DESTINATION_KEY = ""
+    }
+}
---
+++ src/main/kotlin/it/unibo/acdingnet/protelis/util/Extensions.kt
@@ -0,0 +1,5 @@
+package it.unibo.acdingnet.protelis.util
+
+val Float.Companion.SIZE_BYTES: Int get() = 4
+
+val Double.Companion.SIZE_BYTES: Int get() = 8
