5ffbc77ca062d8ca4ef36b6982cd0afa7cd969ee
Placu <andrea.placuzzi@studio.unibo.it>
2019-12-27 14:52:27 +0100

    add wrapper for DingNet

---
+++ src/main/kotlin/it/unibo/acdingnet/protelis/dingnetwrapper/SensorECForDingNet.kt
@@ -0,0 +1,26 @@
+package it.unibo.acdingnet.protelis.dingnetwrapper
+
+import iot.GlobalClock
+import iot.mqtt.MqttClientBasicApi
+import it.unibo.acdingnet.protelis.executioncontext.SensorExecutionContext
+import org.protelis.vm.NetworkManager
+
+class SensorECForDingNet(private val sensorNode: SensorNodeWrapper,
+                         applicationUID: String,
+                         mqttClient: MqttClientBasicApi,
+                         netmgr: NetworkManager) : SensorExecutionContext(sensorNode, applicationUID, mqttClient, netmgr) {
+    private val timer: GlobalClock = sensorNode.timer
+
+    init {
+        execEnvironment.put("leader", (sensorNode.deviceUID.uid as String) == "6007316430752245294")
+    }
+
+    override fun instance(): SensorECForDingNet {
+        return SensorECForDingNet(sensorNode, applicationUID, mqttClient, netmgr)
+    }
+
+    override fun getCurrentTime(): Number {
+        return timer.time.nano / 1E6
+    }
+
+}
---
+++ src/main/kotlin/it/unibo/acdingnet/protelis/dingnetwrapper/SensorNodeWrapper.kt
@@ -0,0 +1,32 @@
+package it.unibo.acdingnet.protelis.dingnetwrapper
+
+import iot.GlobalClock
+import iot.mqtt.MqttClientBasicApi
+import it.unibo.acdingnet.protelis.model.LatLongPosition
+import it.unibo.acdingnet.protelis.model.SensorType
+import it.unibo.acdingnet.protelis.node.SensorNode
+import org.protelis.lang.datatype.impl.StringUID
+import org.protelis.vm.ProtelisProgram
+import java.time.LocalTime
+
+
+class SensorNodeWrapper(protelisProgram: ProtelisProgram,
+                        startingTime: LocalTime?,
+                        sleepTime: Long,
+                        sensorDeviceUID: StringUID,
+                        applicationUID: String,
+                        mqttClient: MqttClientBasicApi,
+                        position: LatLongPosition,
+                        sensorTypes: List<SensorType>,
+                        val timer: GlobalClock,
+                        neighborhood: Set<StringUID>)
+    : SensorNode(protelisProgram, sleepTime, sensorDeviceUID, applicationUID, mqttClient, position, sensorTypes, neighborhood) {
+
+    override fun createContext(): SensorECForDingNet {
+        return SensorECForDingNet(this, applicationUID, mqttClient, networkManager)
+    }
+
+    init {
+        timer.addPeriodicTrigger(startingTime, sleepTime) { runVM() }
+    }
+}
