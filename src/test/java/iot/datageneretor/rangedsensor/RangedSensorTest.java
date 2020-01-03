a52a6af1c20f70e2300bb30d783e3b8e6e9a4ed4
Placu <andrea.placuzzi@studio.unibo.it>
2020-01-03 14:14:31 +0100

    add more test

--- src/test/java/iot/datageneretor/rangedsensor/RangedSensorTest.java
+++ src/test/java/iot/datageneretor/rangedsensor/RangedSensorTest.java
@@ -2,11 +2,12 @@ package iot.datageneretor.rangedsensor;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import datagenerator.SensorDataGenerator;
+import datagenerator.rangedsensor.api.RangeValue;
 import datagenerator.rangedsensor.iaqsensor.AirQualityLevel;
 import datagenerator.rangedsensor.iaqsensor.IAQDataGeneratorSingleton;
 import iot.Characteristic;
 import iot.Environment;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.jxmapviewer.viewer.GeoPosition;
 
@@ -16,22 +17,29 @@ import java.util.Map;
 
 public class RangedSensorTest {
 
+    private static final SensorDataGenerator instance;
+
+    static {
+        new Environment(new Characteristic[10][10], new GeoPosition(5, 5), 100, Map.of(), Map.of());
+        IAQDataGeneratorSingleton.setConfigFilePath("/sensorsConfig/IAQSensorConfig.toml");
+        instance = IAQDataGeneratorSingleton.getInstance();
+    }
+
     @Test
     public void defaultValue() {
-        Characteristic[][] characteristics = new Characteristic[10][10];
-        for (int i = 0; i < 10; i++) {
-            for (int j = 0; j < 10; j++) {
-                characteristics[i][j] = Characteristic.Plain;
-            }
-        }
-        var env = new Environment(characteristics, new GeoPosition(5, 5), 100, Map.of(), Map.of());
-        IAQDataGeneratorSingleton.setConfigFilePath("/sensorsConfig/IAQSensorConfig.toml");
-        var instance = IAQDataGeneratorSingleton.getInstance();
+        checkCell(1, 1, LocalTime.of(0, 0, 30), AirQualityLevel.GOOD); // in the file config GOOD is the default value
+    }
 
-        int value = instance.generateData(1, 1, new GeoPosition(0, 0), LocalTime.of(0, 0, 30))[0];
+    @Test
+    public void changeValue() {
+        checkCell(3, 4, LocalTime.of(0, 0, 1), AirQualityLevel.MODERATE);
+        checkCell(3, 4, LocalTime.of(0, 2, 1), AirQualityLevel.POOR);
+    }
 
-        var def = AirQualityLevel.GOOD; // in the file config GOOD is the default value
-        assertTrue(def.getLowerBound() <= value);
-        assertTrue(def.getUpperBound() >= value);
+    private void checkCell(int x, int y, LocalTime time, RangeValue expectedValue) {
+        int value = instance.generateData(x, y, new GeoPosition(0, 0), time)[0];
+
+        assertTrue(expectedValue.getLowerBound() <= value);
+        assertTrue(expectedValue.getUpperBound() >= value);
     }
 }
