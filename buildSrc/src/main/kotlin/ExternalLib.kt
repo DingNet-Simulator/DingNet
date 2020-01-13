d10ed926f7c2a590023727b29529765c5e2d949d
Placu <andrea.placuzzi@studio.unibo.it>
2020-01-13 11:28:46 +0100

    update version of external library

--- buildSrc/src/main/kotlin/ExternalLib.kt
+++ buildSrc/src/main/kotlin/ExternalLib.kt
@@ -3,10 +3,10 @@ data class ExternalLib(val libBaseName: String, val libVersion: String, val libU
     val libFullName = "$libBaseName-$libVersion.jar"
 
     companion object {
-        val mqtt_client_wrapper: ExternalLib = ExternalLib("MqttClientWrapper", "0.2.1",
-            "https://github.com/Placu95/MqttClientWrapper/releases/download/v0.2.1/MqttClientWrapper-0.2.1.jar")
+        val mqtt_client_wrapper: ExternalLib = ExternalLib("MqttClientWrapper", "0.3.1",
+            "https://github.com/Placu95/MqttClientWrapper/releases/download/v0.3.1/MqttClientWrapper-0.3.1.jar")
 
-        val protelis_over_mqtt: ExternalLib = ExternalLib("ProtelisOverMqtt", "0.1.1",
-            "https://github.com/Placu95/ProtelisOverMqtt/releases/download/v0.1.1/ProtelisOverMqtt-0.1.1.jar")
+        val protelis_over_mqtt: ExternalLib = ExternalLib("ProtelisOverMqtt", "0.2.0",
+            "https://github.com/Placu95/ProtelisOverMqtt/releases/download/v0.2.0/ProtelisOverMqtt-0.2.0.jar")
     }
 }
