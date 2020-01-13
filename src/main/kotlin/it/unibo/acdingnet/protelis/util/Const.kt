15109d860e384843c54044a8cee5f3b19f99099f
Placu <andrea.placuzzi@studio.unibo.it>
2020-01-13 11:42:41 +0100

    reduce range

--- src/main/kotlin/it/unibo/acdingnet/protelis/util/Const.kt
+++ src/main/kotlin/it/unibo/acdingnet/protelis/util/Const.kt
@@ -3,7 +3,7 @@ package it.unibo.acdingnet.protelis.util
 object Const {
 
     const val APPLICATION_ID = "1"
-    const val NEIGHBORHOOD_RANGE = 1000.0
+    const val NEIGHBORHOOD_RANGE = 500.0
     const val MOTT_ADDRESS = "tcp://test.mosquitto.org:1883"
     const val MQTT_CLIENT_ID = "testFenomeno1995-app"
 
