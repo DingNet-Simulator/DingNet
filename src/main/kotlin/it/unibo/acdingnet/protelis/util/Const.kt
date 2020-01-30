75ea25c094267b56c5a448d2149a0a111930208b
Placu <andrea.placuzzi@studio.unibo.it>
2020-01-30 14:52:36 +0100

    add new constants for protelis env

--- src/main/kotlin/it/unibo/acdingnet/protelis/util/Const.kt
+++ src/main/kotlin/it/unibo/acdingnet/protelis/util/Const.kt
@@ -3,7 +3,7 @@ package it.unibo.acdingnet.protelis.util
 object Const {
 
     const val APPLICATION_ID = "1"
-    const val NEIGHBORHOOD_RANGE = 500.0
+    const val NEIGHBORHOOD_RANGE = 1500.0
     const val MOTT_ADDRESS = "tcp://test.mosquitto.org:1883"
     const val MQTT_CLIENT_ID = "testFenomeno1995-app"
 
@@ -11,5 +11,11 @@ object Const {
         const val IAQLEVEL_KEY = "iaqLevel"
         const val SOURCE_KEY = ""
         const val DESTINATION_KEY = ""
+        const val NODE_TYPE = "nodeType"
+        const val SENSOR_TYPE = "sensor"
+        const val BUILDING_TYPE = "building"
+        const val CURRENT_TEMP = "currentTemp"
+        const val DESIRED_TEMP = "desiredTemp"
+        const val PM10 = "pm10"
     }
 }
