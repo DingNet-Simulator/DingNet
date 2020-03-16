f2d1ef155ad604468f237e98bf69e171f00441aa
Placu <andrea.placuzzi@studio.unibo.it>
2020-03-16 16:26:09 +0100

    extract interface

---
+++ src/main/java/application/pollution/PollutionGridImpl.java
@@ -0,0 +1,57 @@
+package application.pollution;
+
+import org.jxmapviewer.viewer.GeoPosition;
+import util.MapHelper;
+import util.Pair;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class PollutionGridImpl implements PollutionGrid {
+    // FIXME synchronized is necessary here, otherwise concurrent modification exceptions are thrown
+    //  (even though the GUI updating should happen synchronously with invokeAndWait)
+
+    // The pollution measurements: for each device, the most recent measurement (location + pollution level) is stored
+    private Map<Long, Pair<GeoPosition, PollutionLevel>> pollutionMeasurements;
+
+
+    public PollutionGridImpl() {
+        pollutionMeasurements = new HashMap<>();
+    }
+
+
+    @Override
+    public void addMeasurement(long deviceEUI, GeoPosition position, PollutionLevel level) {
+        synchronized (this) {
+            this.pollutionMeasurements.put(deviceEUI, new Pair<>(position, level));
+        }
+    }
+
+    @Override
+    public double getPollutionLevel(GeoPosition position) {
+        synchronized (this) {
+            var pollutionAtPosition = pollutionMeasurements.values().stream()
+                .filter(o -> o.getLeft().equals(position))
+                .map(Pair::getRight)
+                .findFirst();
+            if (pollutionAtPosition.isPresent()) {
+                return pollutionAtPosition.get().getPollutionFactor();
+            }
+
+            // Calculate some mean pollution based on the distance of other measurements
+            // NOTE: this does not take the time of the measurement into account, only the distance
+            List<Pair<Double, PollutionLevel>> distances = pollutionMeasurements.values().stream()
+                .map(e -> new Pair<>(MapHelper.distance(e.getLeft(), position), e.getRight()))
+                .collect(Collectors.toList());
+
+            return PollutionLevel.getMediumPollution(distances).getPollutionFactor();
+        }
+    }
+
+    @Override
+    public void clean() {
+        pollutionMeasurements = new HashMap<>();
+    }
+}
--- src/test/java/unit/TestPollutionGridImpl.java
+++ src/test/java/unit/TestPollutionGridImpl.java
@@ -1,6 +1,7 @@
 package unit;
 
 import application.pollution.PollutionGrid;
+import application.pollution.PollutionGridImpl;
 import application.pollution.PollutionLevel;
 import org.junit.jupiter.api.Test;
 import org.jxmapviewer.viewer.GeoPosition;
@@ -8,24 +9,24 @@ import org.jxmapviewer.viewer.GeoPosition;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
-class TestPollutionGrid {
+class TestPollutionGridImpl {
     @Test
     void addMeasurements() {
-        PollutionGrid grid = new PollutionGrid();
+        PollutionGrid grid = new PollutionGridImpl();
 
         grid.addMeasurement(1L, new GeoPosition(5, 5), new PollutionLevel(0.5));
-        assertEquals(grid.getPollutionLevel(new GeoPosition(5, 5)).getPollutionFactor(), 0.5);
+        assertEquals(grid.getPollutionLevel(new GeoPosition(5, 5)), 0.5);
 
         grid.addMeasurement(2L, new GeoPosition(10, 10), new PollutionLevel(0.8));
         grid.addMeasurement(2L, new GeoPosition(10, 10), new PollutionLevel(0.1));
-        assertEquals(grid.getPollutionLevel(new GeoPosition(10, 10)).getPollutionFactor(), 0.1);
+        assertEquals(grid.getPollutionLevel(new GeoPosition(10, 10)), 0.1);
     }
 
     @Test
     void noMeasurements() {
-        PollutionGrid grid = new PollutionGrid();
+        PollutionGrid grid = new PollutionGridImpl();
 
-        assertEquals(grid.getPollutionLevel(new GeoPosition(5, 5)).getPollutionFactor(), 0.0);
-        assertEquals(grid.getPollutionLevel(new GeoPosition(50, 50)).getPollutionFactor(), 0.0);
+        assertEquals(grid.getPollutionLevel(new GeoPosition(5, 5)), 0.0);
+        assertEquals(grid.getPollutionLevel(new GeoPosition(50, 50)), 0.0);
     }
 }
