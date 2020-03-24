4078816ca462aeafff4dcec923e98b1f13a35864
Placu <andrea.placuzzi@studio.unibo.it>
2020-03-24 10:38:09 +0100

    fix bug in byte array conversion due to removeAll

--- src/main/kotlin/it/unibo/acdingnet/protelis/model/sensorconverter/SensorConverter.kt
+++ src/main/kotlin/it/unibo/acdingnet/protelis/model/sensorconverter/SensorConverter.kt
@@ -11,26 +11,22 @@ interface SensorConverter<T> {
 class DefaultConverter : SensorConverter<Double> {
 
     override fun convert(length: Int, data: MutableList<Byte>): Double {
-        var count = 0
-        val pair = data.partition { count++ <length }
-        data.removeAll(pair.first)
-        val buffer = ByteBuffer.wrap(pair.first.toByteArray())
+        val buffer = ByteBuffer.wrap((1..length).map { data.removeAt(0) }.toByteArray())
         return when (length) {
-            Byte.SIZE_BYTES -> buffer.get().toDouble()
+            Byte.SIZE_BYTES -> (buffer.get().toInt() and 0xFF).toDouble()
             Short.SIZE_BYTES -> buffer.short.toDouble()
             Float.SIZE_BYTES -> buffer.float.toDouble()
             Double.SIZE_BYTES -> buffer.double
             else -> throw IllegalStateException("impossible convert. Sensor with unknown length")
         }
     }
+
+
 }
 
 class GPSConverter : SensorConverter<LatLongPosition> {
     override fun convert(length: Int, data: MutableList<Byte>): LatLongPosition {
-        var count = 0
-        val pair = data.partition { count++ < length }
-        data.removeAll(pair.first)
-        val buffer = ByteBuffer.wrap(pair.first.toByteArray())
+        val buffer = ByteBuffer.wrap((1..length).map { data.removeAt(0) }.toByteArray())
         return LatLongPosition(buffer.float.toDouble(),
             buffer.getFloat(Float.SIZE_BYTES).toDouble())
     }
