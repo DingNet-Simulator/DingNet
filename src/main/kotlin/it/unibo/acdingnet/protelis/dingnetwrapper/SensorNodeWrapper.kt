c63108b62cdae8e8805cfd4644c5469d68d568c2
Placu <andrea.placuzzi@studio.unibo.it>
2020-03-24 10:32:42 +0100

    add method get pollution value

--- src/main/kotlin/it/unibo/acdingnet/protelis/dingnetwrapper/SensorNodeWrapper.kt
+++ src/main/kotlin/it/unibo/acdingnet/protelis/dingnetwrapper/SensorNodeWrapper.kt
@@ -3,6 +3,7 @@ package it.unibo.acdingnet.protelis.dingnetwrapper
 import iot.GlobalClock
 import it.unibo.acdingnet.protelis.model.SensorType
 import it.unibo.acdingnet.protelis.node.SensorNode
+import it.unibo.acdingnet.protelis.util.Const
 import it.unibo.mqttclientwrapper.api.MqttClientBasicApi
 import it.unibo.protelisovermqtt.model.LatLongPosition
 import org.protelis.lang.datatype.impl.StringUID
@@ -31,4 +32,7 @@ class SensorNodeWrapper(
     init {
         timer.addPeriodicTrigger(startingTime, sleepTime) { runVM() }
     }
+
+    //default value for sensors that have not receive value yet
+    fun getPollutionValue(): Double = executionContext.executionEnvironment.get(Const.ProtelisEnv.IAQLEVEL, 40.0) as Double
 }
